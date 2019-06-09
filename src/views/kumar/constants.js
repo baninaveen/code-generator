65ef425982230f46ba224f31b735873d37bf8181
naveen <baninkumar@gmail.com>
2019-06-09 20:02:48 +0530

    final commit

--- src/views/kumar/constants.js
+++
@@ -1,2 +0,0 @@
-export const ACTION_TYPE = "write_unique_name"
-export const ACTION_TYPE = "write_unique_name"
--- src/views/testall/actions.js
+++
@@ -1,8 +0,0 @@
-import { ACTION_TYPE } from "./constants"
-
-export function actionFunctionName(payload) {
-  return {
-    type: ACTION_TYPE,
-    payload
-  }
-}
